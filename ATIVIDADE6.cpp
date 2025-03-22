b4186b0383fe7011c49f12d90e3658eb5a6bba65
Caíque Kubo <caiquevks@gmail.com>
2025-03-21 21:35:40 -0300

    ATUALIZADO ATV 6

--- ATIVIDADE6.cpp
+++ ATIVIDADE6.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main () {
     string nome;
-    double valor_mensal, valor_vendas, lucro, final, porcentagem = 0.15;
+    double valor_mensal, valor_vendas, lucro, final, porcentagem = 0.15, aumento;
 
     cout << "Digite o nome do Vendedor:" << endl;
     getline(cin,nome);
@@ -16,9 +16,11 @@ int main () {
     //CALCULO
     lucro = valor_vendas * porcentagem;
     final = valor_mensal + lucro;
+    aumento = final / valor_mensal - 1 * 100;
 
     cout << "O valor da porcentagem da comissão é de R$" << lucro << ",00" << endl;
     cout << "O salário final do vendedor " << nome << " é de R$" << final << ",00" << endl;
+    cout << "Teve um aumento de " << aumento << "%." << endl;
 
     system("pause");
 
---
+++ TESTE.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+
+    cout << "╔════════════════════════════════════╗" << endl;
+    cout << "║                                    ║" << endl;
+    cout << "║         AESTHETIC PROGRAM          ║" << endl;
+    cout << "║                                    ║" << endl;
+    cout << "╠════════════════════════════════════╣" << endl;
+    cout << "║  Digite seu nome:                  ║" << endl;
+    cout << "║  Digite ║" << endl;
+
+
+
+
+}

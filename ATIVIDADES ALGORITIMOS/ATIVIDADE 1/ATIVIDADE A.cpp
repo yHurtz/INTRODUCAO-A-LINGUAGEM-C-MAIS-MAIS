ab0249425b48ef62d7d02f7b009772319dfafa23
Caíque Kubo <caiquevks@gmail.com>
2025-04-04 22:19:23 -0300

    atividade algoritimos A

---
+++ ATIVIDADES ALGORITIMOS/ATIVIDADE 1/ATIVIDADE A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+float A, B, C, D, E;
+    cout << "Digite a letra A:";
+    cin >>  A;
+    cout << "Digite a letra B:";
+    cin >> B;
+    cout << "Digite a letra C:";
+    cin >> C;
+    cout << "Digite a letra D:";
+    cin >> D;
+    cout << "Digite a letra E:";
+    cin >> E;
+
+
+    if (A == 2) {
+    cout << "Laranja" << endl;
+        if (B == 5) {
+            cout << "Uva" << endl;
+            cout << "Melancia" << endl;
+    }
+        if (D == 3) {
+        cout << "Pêra" << endl;
+ }  else {
+        cout << "Morango"<< endl;
+    }
+} else {
+    if ( C == 3) {
+    cout << "Limão"<< endl;
+ }
+    if ( E == 4) {
+    cout << "Abacate"<< endl;
+    cout << "Abacaxi"<< endl;
+ }
+}
+
+    cout << "pitanga"<< endl;
+
+    system("pause");
+}

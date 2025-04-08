7ed601120cdf8fbf4475efb72c7ff783deaabcde
Caíque Kubo <caiquevks@gmail.com>
2025-04-08 20:58:38 -0300

    ATIVIDADE 1 ALGORITIMOS

---
+++ ATIVIDADES ALGORITIMOS/ATIVIDADES/ATIVIDADE2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main () {
+    float num;
+
+    cout << "Digite um número inteiro:" << endl;
+    cin << num;
+        if (num % 2 == 0) {
+            cout << num << "se trata de um número par." << endl;
+            else if(num % 2 !=== 0)
+        }
+}

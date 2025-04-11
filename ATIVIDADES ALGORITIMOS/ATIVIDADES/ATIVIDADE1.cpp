2a7f3981fa6c644f4ddd44466a23485a8460df4e
Caíque Kubo <caiquevks@gmail.com>
2025-04-11 12:04:58 -0300

    ATUALIZADO ATIVIDADE 1 ALGORITIMOS

--- ATIVIDADES ALGORITIMOS/ATIVIDADES/ATIVIDADE1.cpp
+++ ATIVIDADES ALGORITIMOS/ATIVIDADES/ATIVIDADE1.cpp
@@ -4,16 +4,23 @@ using namespace std;
 
 int main (){
     double n1, n2, n3, n4, media;
+    double tot_faltas, max_faltas = 12;
 
     cout << "Digite as 4 notas bimestrais" << endl;
     cin >> n1 >> n2 >> n3 >> n4;
+    cout << "Digite quantas faltas você tem" << endl;
+    cin >> tot_faltas;
+
+
     media = (n1 + n2 + n3 + n4) / 4;
     cout << "Media do aluno: " << media << endl;
 
-    if (media >= 6.0) {
+    if ((media >= 6.0) && (tot_faltas <= max_faltas)) {
         cout << "Parabéns, você passou!" << endl;
-    } else {
-        cout << "Desculpe, você não passou pois não alcançou a média." << endl;
+    } else if ((media < 6.0) && (tot_faltas <= max_faltas)) {
+        cout << "Reprovato por nota." << endl;
+    } else if ((media >= 6.0) && (tot_faltas > max_faltas)) {
+        cout << "Reprovado por faltas." << endl;
     }
     system("pause");
 }
